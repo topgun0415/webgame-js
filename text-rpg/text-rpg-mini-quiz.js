11206cc47fec4317611cca6282020ecd7f62c89e
philipuuu <topgun0415@naver.com>
2022-03-07 16:32:07 +0900

    Fix : javascript class extends quiz add

--- text-rpg/text-rpg-mini-quiz.js
+++ text-rpg/text-rpg-mini-quiz.js
@@ -14,5 +14,29 @@ class Human {
 }
 
 const me = new Human('philip', 30);
-me.showName();
-me.showAge();
+me.showName(); // hello my name is philip
+me.showAge(); // hello I'm 30 years old
+
+// extends를 사용해서 상속을 받는다.
+
+class Programmer extends Human {
+  constructor(name, age, languages) {
+    super(name, age);
+
+    this.languages = languages;
+  }
+  writeCode() {
+    console.log(`I can do ${this.languages}`);
+  }
+}
+
+// Prameter로 배열을 넣을 수도 있음
+const programmer = new Programmer('Philipuuu', 29, [
+  'html',
+  'css',
+  'javascript',
+]);
+
+programmer.showName(); // hello my name is Philipuuu
+programmer.showAge(); // hello I'm 29 years old
+programmer.writeCode(); // I can do html,css,javascript
